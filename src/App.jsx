b484a645cd6f964feed5f47f2d8105bdbb268e44
Vedant Pandit <vedantpandit@Vedants-MacBook-Pro.local>
2017-02-17 19:16:11 -0800

    Additional information added to profile component along with styling

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ class App1 extends Component{
   search(){
    console.log('this.state',this.state);
     const BASE_URL = 'https://api.spotify.com/v1/search?';
-  //  const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
+  //const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
     let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-    //console.log("FETCH_URL",FETCH_URL);
+    console.log("FETCH_URL",FETCH_URL);
     fetch(FETCH_URL, {
       method: 'GET'
     })
@@ -51,7 +51,7 @@ class App1 extends Component{
                            }
                }}
              />
-           <InputGroup.Addon onClick={() => this.search()}>
+             <InputGroup.Addon onClick={() => this.search()}>
                <Glyphicon glyph="search"></Glyphicon>
              </InputGroup.Addon>
            </InputGroup>
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -13,9 +13,28 @@ class Profile extends Component {
     artist = this.props.artist !== null ? this.props.artist : artist; // set the value of artist if it has data
 
     return (
-      <div>
-        <div>{artist.name}</div>
-        <div>{artist.followers.total} followers</div>
+      <div className='profile'>
+        <img
+          alt="Profile"
+          className="profile-img"
+          src={artist.images[0].url}
+        />
+        <div className="profile-info">
+          <div className="profile-name">{artist.name}</div>
+          <div className="profile-followers">{artist.followers.total} followers</div>
+          <div className="profile-genres">
+              {
+                artist.genres.map((genre, key) => {
+                  genre = genre !== artist.genres[artist.genres.length-1] // checking whether the genre item is last in array or not. if last, donot add comma, else add
+                              ? ` ${genre},`
+                              : ` ${genre}`;
+                  return (
+                    <span key={key}>{genre}</span>
+                  )
+                })
+              }
+            </div>
+        </div>
       </div>
     )
   }
