8ac0ddce479fc98b50fd7b52e02200b5a8cf835d
Vedant Pandit <vedantpandit@Vedants-MacBook-Pro.local>
2017-02-18 20:46:21 -0800

    Play functionality added, some styling remaining

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,14 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playingUrl: '',
+      audio: null,
+      playing: false
+    }
+   }
+
+  playAudio(previewUrl) {
+   let audio = new Audio(previewUrl);
+   if (!this.state.playing) {
+    audio.play();
+    this.setState({
+      playing: true,
+      playingUrl: previewUrl,
+      audio
+    })
+  } else { // audio already playng
+    if (this.state.playingUrl === previewUrl) { // pausing the same audio
+      this.state.audio.pause();
+      this.setState({
+        playing: false
+      })
+    } else { // pausing the same audio and playing another track(audio)
+      this.state.audio.pause();
+      audio.play();
+      this.setState({
+       playing: true,
+       playingUrl: previewUrl,
+       audio
+     })
+    }
+   }
+  }
+
   render(){
-    console.log('gallery props', this.props);
+    console.log('gallery props', this.props); // tracks={this.state.tracks} passed as props from App.jsx
     const tracks = this.props.tracks;
 
     return(
       <div>
         {
         tracks.map((track, k) => {
+          console.log('track',track);
           const trackImg = track.album.images[0].url;
           return (
             <div
@@ -20,6 +57,7 @@ class Gallery extends Component {
                 src={trackImg}
                 className="track-img"
                 alt="track"
+                onClick={() => this.playAudio(track.preview_url)}
               />
 
               <p className="track-text">
